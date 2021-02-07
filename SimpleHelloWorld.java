11777d377a78e1d623ddff106f08f5df7cea9cd0
Lucas Olejarz <olejarzlucas@gmail.com>
2021-02-07 17:00:30 +0100

    Update SimpleHelloWorld.java

--- SimpleHelloWorld.java
+++ SimpleHelloWorld.java
@@ -1,5 +1,5 @@
 public class SimpleHelloWorld {
   public static void main(String[] args) {
-    System.out.println("Execution de Jenkins");
+    System.out.println("Modif Execution de Jenkins");
   }
 }

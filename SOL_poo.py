915cde4e24cda83406738430703b221b0b137a1c
Lucas Olejarz <olejarzlucas@gmail.com>
2021-02-12 11:40:55 +0100

    Add files via upload

---
+++ SOL_poo.py
@@ -0,0 +1,29 @@
+import unittest
+
+from poo import Personne
+
+
+class TestSum(unittest.TestCase):
+
+    def test_creation_compte(self):
+        """
+        Vérifie que la création d'un compte fonctionne
+        """
+        personne = Personne('Jean','Jacques')
+        personne.creer_compte_bancaire()
+
+        self.assertEqual(personne.compte_bancaire.solde, 0)
+    def test_depot_compte(self):
+        personne = Personne('Jean','Jacques')
+        personne.creer_compte_bancaire()
+        personne.depot(1000)
+        self.assertEqual(personne.compte_bancaire.solde, 1000)
+
+    def test_retrait_compte(self):
+        personne = Personne('Jean','Jacques')
+        personne.creer_compte_bancaire()
+        personne.retrait(20)
+        self.assertEqual(personne.compte_bancaire.solde, -20)
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ poo.py
@@ -0,0 +1,39 @@
+class Personne:
+    """docstring for Personne."""
+
+    def __init__(self, nom, prenom):
+        self.nom = nom
+        self.prenom = prenom
+        self.compte_bancaire = None
+
+    def creer_compte_bancaire(self):
+        self.compte_bancaire = CompteBancaire(self.nom)
+
+    def depot(self, somme):
+        self.compte_bancaire.depot(somme)
+    def solde(self):
+        self.compte_bancaire.affiche();
+    def retrait(self,somme):
+        self.compte_bancaire.retrait(somme)
+
+class CompteBancaire:
+
+    def __init__(self,nom='Dupont'):
+        """ création du constructeur de la classe avec les valeurs par defaut 'Dupont' et 0 """
+        self.nom=nom
+        self.solde=0
+
+    def depot(self,somme):
+        """ ajout d'une somme à l'attribut solde """
+
+        self.solde+=somme
+
+    def retrait(self,somme):
+        """ retrait d'une somme à l'attribut solde """
+
+        self.solde-=somme
+
+    def affiche(self):
+        """ L'affichage des informations d'un compte"""
+
+        print("Le solde du compte bancaire de %s est de %.2f euros."%(self.nom,self.solde))
